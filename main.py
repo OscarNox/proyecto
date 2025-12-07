8aaaa226dc0e9bdcdd39ea799b38be6e2dd5217e
OscarNox <jerezsantosjoseoscar@gmail.com>
2025-12-06 23:43:11 -0400

    avance del curso

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hola mundo")
+#esto es una asignacion
+mensaje="hola"
+mensaje += " " 
+mensaje += "jose oscar"
+print(mensaje)
+
+#esto es una concatenacion
+mensaje="Hola"
+espacio=" "
+nombre="Jose Oscar"
+print(mensaje + espacio + nombre)
+
+#concatenacion con numeros
+numero_uno=5
+numero_dos=10
+resultado= numero_uno + numero_dos
+resultado=str(resultado)
+print("El resultado de la suma es: " + resultado) 
+
+
