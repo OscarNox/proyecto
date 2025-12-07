266f276e1c7a6c70b9c1b3cb1aef485ba3e5d2d9
OscarNox <jerezsantosjoseoscar@gmail.com>
2025-12-06 23:53:57 -0400

    clasificacion

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 #esto es una asignacion
+print("-----Asignacion-----")
 mensaje="hola"
 mensaje += " " 
 mensaje += "jose oscar"
 print(mensaje)
 
 #esto es una concatenacion
+print("-----Concatenacion-----")
 mensaje="Hola"
 espacio=" "
 nombre="Jose Oscar"
 print(mensaje + espacio + nombre)
 
 #concatenacion con numeros
+print("-----Concatenacion con numeros-----")   
 numero_uno=5
 numero_dos=10
 resultado= numero_uno + numero_dos
 resultado=str(resultado)
 print("El resultado de la suma es: " + resultado) 
 
+print("-----Busqueda-----")
 #esto es una busqueda
 mensaje="Hola Jose Oscar"
 buscar_subcadena=mensaje.find("Oscar")
