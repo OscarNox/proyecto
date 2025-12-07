a6a9726948f172195474abe3e880df0b6f6c80de
OscarNox <jerezsantosjoseoscar@gmail.com>
2025-12-07 00:15:58 -0400

    avance del curso

---
+++ palabras_reservadas_en_python.py
@@ -0,0 +1,10 @@
+#estas son las palabras reservadas en python
+#and, as, assert, break, class, continue, def, del, elif, else, except, False, finally, for, from, global, if, import, in, is, lambda, None, nonlocal, not, or, pass, raise, return, True
+#try, while, with, yield
+#palabras reservadas en python 3.10 y versiones posteriores
+
+prinT=26
+Print=21
+resultado= prinT + Print
+resultado=str(resultado)
+print("El resultado de la suma es: " + resultado)
