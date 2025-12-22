a124c45cc0f1e39ef8462ed2b3aae8a1ecfaeecd
OscarNox <jerezsantosjoseoscar@gmail.com>
2025-12-22 11:18:02 -0400

    agregue tipos de datos

---
+++ tipos_de_datos.py
@@ -0,0 +1,23 @@
+#tipos de datos enteros o largos 
+numero = 26
+print(numero, type(numero))
+
+#tipos de datos flotantes
+numero_flotante = 21.5
+print(numero_flotante, type(numero_flotante))
+
+#tipo de dato Numero complejo
+numero_complejo = 2 + 1j
+print(numero_complejo, type(numero_complejo))
+
+#tipo de dato String
+nombre = 'paola'
+print(nombre, type(nombre))
+
+#tipo de dato Booleano
+verdadero_falso = 'paola' == 'Paola'
+print(verdadero_falso, type(verdadero_falso))
+
+verdadero_falso = 'jose' == 'jose'
+print(verdadero_falso, type(verdadero_falso))
+
