f26fb9c31f049407fb53e8fb204ac95f7866ecaa
OscarNox <jerezsantosjoseoscar@gmail.com>
2025-12-22 11:53:52 -0400

    agregue entrada de datos

---
+++ entrada_de_datos.py
@@ -0,0 +1,10 @@
+palabra = input("introduce una palabra: ")
+num_int = int(input("introduce un número entero: "))
+num_float = float(input("introduce un número flotante: "))
+num_complex = complex(input("introduce un número complejo: "))
+
+print("String: ", palabra)
+print("Entero: ", num_int)
+print("Flotante: ", num_float)
+print("Numero complejo: ", num_complex)
+
---
+++ entrada_de_datos2.py
@@ -0,0 +1,8 @@
+nombre = input("cual es tu nombre?: ")
+print("hola ", nombre, ", vamos a realizar una suma.")
+
+num_uno = int(input("introduce el primer número entero: "))
+num_dos = int(input("introduce el segundo número entero: "))
+resultados = num_uno + num_dos
+print(nombre + " el resultado de la suma es: ", resultados)
+
